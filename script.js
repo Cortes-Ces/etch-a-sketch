e087822c593363a1cd57460a0d3e106869dcc66e
Cortes-Ces <Cortesces90@gmail.com>
2023-09-10 22:14:59 -0400

    Fixed and added black, gray, rgb, and reset buttons

--- script.js
+++ script.js
@@ -3,19 +3,91 @@ const btnBlack = document.createElement('button')
 const btnGray = document.createElement('button')
 const btnRGB = document.createElement('button')
 const btnSize = document.createElement('button')
-const buttonsContainer = document.createElement('buttons')
+const btnReset = document.createElement('button')
+const section = document.querySelector('.section')
+const buttonsContainer = document.querySelector('.buttons')
 
 // Create grid dynamically using divs
 function createDivs(col, rows) {
+    
     for(let i = 0; i < (col * rows); i++){
         const div = document.createElement('div')
-        container.style.border = '1px solid black';
-        div.style.border = '1px solid '
-        container.style.gridTemplateColumns = `repeat(${col}, ifr)`;
-        container.style.gridTemplateRows = `repeat(${rows}, ifr)`;
+        div.style.border = '1px solid black'
+        container.style.gridTemplateColumns = `repeat(${col}, 1fr)`;
+        container.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
         container.appendChild(div).classList.add('box')
     }
 }
 
-createDivs(16, 16)
+
+createDivs(16,16)
+
+function blackColor() {
+    const boxs = container.querySelectorAll('.box')
+    btnBlack.className = 'btn btn-dark';
+    btnBlack.textContent = 'Black';
+    btnBlack.addEventListener('click', () =>{
+        boxs.forEach(box => box.addEventListener('mouseover', () => {
+            //let Rnum = Math.floor(Math.random() * 25);
+            let blackScale = `rgb(${0}, ${0}, ${0})`
+            box.style.background = blackScale;
+        }))
+    })
+
+    buttonsContainer.appendChild(btnBlack)
+}
+blackColor()
+
+function grayColor() {
+    const boxs = container.querySelectorAll('.box')
+    btnGray.className = 'btn btn-secondary';
+    btnGray.textContent = 'Gray';
+    btnGray.addEventListener('click', () =>{
+        boxs.forEach(box => box.addEventListener('mouseover', () => {
+            let Rnum = Math.floor(Math.random() * 256);
+            let grayScale = `rgb(${Rnum}, ${Rnum}, ${Rnum})`
+            box.style.background = grayScale;
+        }))
+    })
+
+    buttonsContainer.appendChild(btnGray)
+}
+grayColor()
+
+function rgbColor() {
+    const boxs = container.querySelectorAll('.box')
+    btnRGB.className = 'btn btn-warning';
+    btnRGB.textContent = 'Random';
+    btnRGB.addEventListener('click', () =>{
+        boxs.forEach(box => box.addEventListener('mouseover', () => {
+            let r = Math.floor(Math.random() * 255);
+            let g = Math.floor(Math.random() * 255);
+            let b = Math.floor(Math.random() * 255);
+            let randomScale = `rgb(${r}, ${g}, ${b})`
+            box.style.background = randomScale;
+        }))
+    })
+
+    buttonsContainer.appendChild(btnRGB)
+}
+rgbColor()
+
+function resetBtn() {
+    const boxs = container.querySelectorAll('.box')
+    btnReset.className = 'btn btn-primary';
+    btnReset.textContent = 'Reset';
+    btnReset.addEventListener('click', () => {
+        boxs.forEach(box => {
+            box.style.background = `rgb(${255}, ${255}, ${255})`
+        })
+    })
+
+    
+
+
+buttonsContainer.appendChild(btnReset)
+}
+
+    
+resetBtn()
 
